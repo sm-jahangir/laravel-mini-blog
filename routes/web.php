4c17bbbc5f1a7c8d6468aab44975757eba6305e1
sm-jahangir <samraatjahangir@gmail.com>
2021-10-09 11:17:24 +0600

    Admin Template Mastering

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::view('admin', 'layouts.backend.app');
+Route::get('admin', [DashboardController::class, 'dashboard']);
