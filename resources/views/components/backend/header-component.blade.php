e2eb5c184ca5d34069f711051240aae13ad94e3c
sm-jahangir <samraatjahangir@gmail.com>
2021-10-13 12:17:12 +0600

    Spatie Role and Permission Successfull

--- resources/views/components/backend/header-component.blade.php
+++ resources/views/components/backend/header-component.blade.php
@@ -126,14 +126,18 @@
             </div>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                <i class="fas fa-expand-arrows-alt"></i>
+            <a class="nav-link" href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+                <i class="fas fa-sign-out-alt"></i>
             </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                <i class="fas fa-th-large"></i>
-            </a>
+
+         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+             @csrf
+         </form>
+
+
+
         </li>
     </ul>
 </nav>
